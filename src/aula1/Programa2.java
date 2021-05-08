f1231baddbb89938c1281cb6f2795e94deb0153a
Fernando Guedes <fernando.jobs@live.com>
2021-05-08 00:08:52 -0300

    concluindo aula prática 1

--- src/aula1/Programa2.java
+++ src/aula1/Programa2.java
@@ -9,12 +9,39 @@ public class Programa2 {
         /**
          * Exercicio 2: Ajude Leônidas
          */
+
         Scanner teclado = new Scanner(System.in);
+        int valorCorreto = 10000;
 
+        System.out.println("Digite o seu palpite");
+        int palpite = teclado.nextInt();
 
+        // solução sem If ternário
+        while (palpite != valorCorreto) {
 
+            if (palpite < valorCorreto) {
+                System.out.println("Um pouco mais...");
+            } else {
+                System.out.println("Um pouco menos...");
+            }
+            System.out.println("Digite o próximo palpite");
+            palpite = teclado.nextInt();
+        }
+        System.out.println("Está correto");
+
+        //---------------------------------------------------------------
+
+        // solução com If ternário
+        String mensagem;
+        while (palpite != valorCorreto) {
+            mensagem = palpite < valorCorreto ? "Um pouco mais..." : "Um pouco menos...";
+            System.out.println(mensagem);
+            System.out.println("Digite o próximo palpite");
+            palpite = teclado.nextInt();
+        }
+        System.out.println("Está correto");
 
         teclado.close();
     }
-
 }
+
---
+++ src/aula1/Programa3.java
@@ -0,0 +1,40 @@
+package aula1;
+
+import java.util.*;
+
+public class Programa3 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        /**
+         * Exercicio 3: Leia uma seqência de Strings
+         */
+        List<String> nomes = new ArrayList<>();
+        String nome;
+
+        System.out.println("Digite quantos nomes você quer inserir: ");
+        int total = teclado.nextInt();
+
+        System.out.println("Digite os nomes: ");
+
+        for (int i = 0; i < total; i++) {
+            nome = teclado.next();
+            nomes.add(nome);
+        }
+
+        System.out.println("=============================");
+
+        System.out.println("Ordem normal...");
+        System.out.println(nomes);
+
+        System.out.println("=============================");
+        System.out.println("Ordem reversa...");
+        Collections.reverse(nomes);
+        System.out.println(nomes);
+        System.out.println("=============================");
+
+        // fechando o Scanner
+        teclado.close();
+    }
+}
